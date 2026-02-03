36423fa6b2b11bd8885ca46c310ef758433f63d6
Gamaliel Aji <132375835+OrionAji@users.noreply.github.com>
2026-02-03 15:03:21 +0100

    Created book catalog and formatted for easy search

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Book
+
+# Customizing the admin interface for the Book model
+class BookAdmin(admin.ModelAdmin):
+    # Display these fields in the list view
+    list_display = ("title", "author", "publication_year")
+    
+    # Enable filtering by these fields in the sidebar
+    list_filter = ("author", "publication_year")
+    
+    # Enable searching by title
+    search_fields = ("title",)
+
+# Register the model with the custom class
+admin.site.register(Book, BookAdmin)
