3c5be86cfbdaf0e9614c7b79a20aba2b641f32c2
Gamaliel Aji <132375835+OrionAji@users.noreply.github.com>
2026-02-25 01:59:42 +0100

    Aha

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -36,3 +36,13 @@ urlpatterns = [
     path('edit_book/<int:book_id>/', views.edit_book, name='edit-book'),
     path('delete_book/<int:book_id>/', views.delete_book, name='delete-book'),
 ]
+
+
+from django.urls import path
+from .views import admin_view, librarian_view, member_view
+
+urlpatterns = [
+    path('admin_view/', admin_view, name='admin_view'),
+    path('librarian_view/', librarian_view, name='librarian_view'),
+    path('member_view/', member_view, name='member_view'),
+]
