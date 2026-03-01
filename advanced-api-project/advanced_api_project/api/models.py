6b409b35e800eef1e89735ddbe10bb80c13bf762
Gamaliel Aji <132375835+OrionAji@users.noreply.github.com>
2026-03-01 02:11:20 +0100

    Added api to installed apps

--- advanced-api-project/advanced_api_project/api/models.py
+++ advanced-api-project/advanced_api_project/api/models.py
@@ -20,7 +20,7 @@ class Book(models.Model):
     links each book to one author via ForeignKey.
     """
     title = models.CharField(max_length=200)
-    publication_year = models.IntegerField()
+    publication_year = models.IntegerField(default=2000)
     author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
 
     def __str__(self):
