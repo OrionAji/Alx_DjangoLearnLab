0b4825f8e6b2e4ff7b4d38276ae110333a85786a
Gamaliel Aji <132375835+OrionAji@users.noreply.github.com>
2026-03-01 01:59:24 +0100

    Hmm

--- advanced-api-project/advanced_api_project/api/models.py
+++ advanced-api-project/advanced_api_project/api/models.py
@@ -2,19 +2,26 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
-from django.contrib.auth.models import User
-from django.utils import timezone
+import datetime
 
 class Author(models.Model):
+    """
+    Author model to store the name of book authors.
+    One Author can have many Books.
+    """
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
 class Book(models.Model):
+    """
+    Book model stores title, publication year, and
+    links each book to one author via ForeignKey.
+    """
     title = models.CharField(max_length=200)
     publication_year = models.IntegerField()
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
-    
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+
     def __str__(self):
         return self.title
